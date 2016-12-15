18f5b8a276aa858007d4b609f2bb792eb7fa3e84
Pelusa901 <alicia.saunders@questu.ca>
2016-12-14 21:41:28 -0800

    skiff

--- SaundersFrancis.AliciaDevon.GLM.R
+++ SaundersFrancis.AliciaDevon.GLM.R
@@ -2,6 +2,7 @@
 # GLM
 wd <- getwd() # get working directory
 setwd("~/Desktop/GitHub/DA.GLM.fish") #Alicia's Directory
+
 fish <- read.csv("data.fish.csv", stringsAsFactors = FALSE) # read data file into object called fish
 View(fish) # view the data 
 
@@ -14,6 +15,7 @@ f1 <- fish[fish$year == 2011, ]
 
 # visualize the relationship between fish age and number of eggs 
 plot(fish$age[fish$year == "2011"], fish$number.eggs[fish$year == "2011"], col = "red", xlab = "Age of fish (yrs)", ylab = "# of eggs")
+plot(f1$age, f1$number.eggs, col = "red", xlab = "Age of fish (yrs)", ylab = "# of eggs")
 
 hist(f1$number.eggs, col = "Purple3", xlab = "# of eggs", main = "Number of Eggs per Fish", las = 1)
 
@@ -22,5 +24,9 @@ f2 <- f1[f1$number.eggs > 0, ]
 View(f2)
 
 hist(f2$number.eggs, col = "Purple3", xlab = "# of eggs", main = "Number of Eggs per Fish", las = 1)
+plot(f2$age, f2$number.eggs)
 
-#-------- Devon's Section---------
+#-------- Devon's LM---------
+
+
+#-------- Devon's Section---------#-------- GLM---------
