79d2db49cd3eb30f4658d7fcb92b081fbcb0ae7a
Pelusa901 <alicia.saunders@questu.ca>
2016-12-14 20:49:56 -0800

    ytdshgn

--- SaundersFrancis.AliciaDevon.GLM.R
+++ SaundersFrancis.AliciaDevon.GLM.R
@@ -1,5 +1,6 @@
+#------ Alicia's Section-------
 # GLM
-setwd("~/Desktop/GitHub/GLM") #Alicia's Directory
+setwd("~/Desktop/GitHub/DA.GLM.fish") #Alicia's Directory
 fish <- read.csv("data.fish.csv", stringsAsFactors = FALSE)
 View(fish)
 
@@ -12,4 +13,8 @@ f1 <- fish[fish$year == 2011, ]
 
 plot(fish$age[fish$year == "2011"], fish$number.eggs[fish$year == "2011"], col = "red", xlab = "Age of fish (yrs)", ylab = "# of eggs")
 
+hist(f1$number.eggs, col = "Purple3", xlab = "# of eggs", main = "Number of Eggs per Fish", las = 1)
 
+
+
+#-------- Devon's Section---------
